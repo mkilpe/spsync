5dba69158c7f708edc69743e46034f4c55218927
Mikael Kilpeläinen <mikael@securepath.fi>
2019-05-28 21:58:15 +0300

    start net comm

---
+++ spsync/comm/comm.cpp
@@ -0,0 +1,5 @@
+#include "comm.hpp"
+
+namespace securepath::sync {
+
+}
---
+++ spsync/comm/comm.hpp
@@ -0,0 +1,37 @@
+#ifndef SPSYNC_COMM_COMM_HEADER
+#define SPSYNC_COMM_COMM_HEADER
+
+#include "interface.hpp"
+
+#include <memory>
+
+namespace securepath::sync {
+
+/**
+ * The default implementation of the comm_input interface to talk with the server
+ */
+class comm : public comm_input {
+public:
+	comm();
+	~comm();
+
+	/// Set the comm_output interface which is used to communicate with higher layer
+	void set_output(comm_output&);
+
+protected:
+
+	// -- comm_input interface, see interface.hpp --
+	virtual sequence_number current_sequence_number() const;
+	virtual request_handle fetch_records(sequence_number start, sequence_number end);
+	virtual request_handle fetch_data(sequence_number record);
+	virtual request_handle commit_record(record_handle);
+	virtual sync::progress& progress();
+	virtual record_storage& records();
+private:
+	class impl;
+	std::unique_ptr<impl> impl_;
+};
+
+}
+
+#endif
