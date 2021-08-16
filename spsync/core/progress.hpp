69a8585eff88bd47b642f24502b21d32dc22454e
Mikael Kilpeläinen <mikael@securepath.fi>
2021-08-16 12:37:53 +0300

    added test keys

--- spsync/core/progress.hpp
+++ spsync/core/progress.hpp
@@ -1,13 +1,17 @@
 #ifndef SPSYNC_CORE_PROGRESS_HEADER
 #define SPSYNC_CORE_PROGRESS_HEADER
 
+#include <securepath/event_system/event_handler.hpp>
+
 namespace securepath::sync {
 
 /**
  * Base interface for progress in the spsync system (eg. download/upload progress, record committing)
  *
  */
-struct progress {
+struct progress : event_system::event_handler {
+    progress(event_system::event_loop&);
+
 	virtual ~progress() = default;
 };
 
