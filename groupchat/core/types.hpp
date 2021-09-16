8914046b0deec35733b95b5480b1cccc50856b53
Mikael Kilpeläinen <mikael@securepath.fi>
2021-09-16 11:29:56 +0300

    add op== required by testing

--- groupchat/core/types.hpp
+++ groupchat/core/types.hpp
@@ -29,10 +29,13 @@ struct host_port {
 	std::uint16_t port;
 
 	//auto operator<=>(host_port const&) const = default;
-	//not working on ndk 23 with clang
+	//not working on android ndk 23 with clang
 
 	bool operator<(host_port const& v) const {
 		return host < v.host || (host == v.host && port < v.port);
+	}
+	bool operator==(host_port const& v) const {
+		return host == v.host && port == v.port;
 	}
 
 	template<typename Ar>
