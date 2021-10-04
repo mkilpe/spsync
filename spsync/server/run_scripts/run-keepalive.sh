145a97eb75ddac160fb122bac2665dde19a0a781
Mikael Kilpeläinen <mikael@securepath.fi>
2021-10-04 15:49:49 +0300

    fix keepalive script

--- spsync/server/run_scripts/run-keepalive.sh
+++ spsync/server/run_scripts/run-keepalive.sh
@@ -2,11 +2,11 @@
 
 while [ -x "./spsync_server" ] ; do
 	"./spsync_server" &
-	pid = $!
+	pid=$!
 	echo $pid > "./spsync_server.pid"
 	wait $pid
-	timestamp = $(date +%s)
-	ret = $?
-	echo "spsync_server exited with ${ret} [pid=${pid}, time=${timestamp}" >> "./exit.log"
+	timestamp=$(date +%s)
+	ret=$?
+	echo "spsync_server exited with ${ret} [pid=${pid}, time=${timestamp}]" >> "./exit.log"
 	sleep 10
 done
